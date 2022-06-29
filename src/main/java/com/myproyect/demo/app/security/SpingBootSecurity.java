da25f8726e9ed8298ab9a21106d0d4f1dfb4be50
abc <abc@example.com>
2022-06-28 22:00:40 -0500

    first commit heroku

--- src/main/java/com/myproyect/demo/app/security/SpingBootSecurity.java
+++ src/main/java/com/myproyect/demo/app/security/SpingBootSecurity.java
@@ -26,12 +26,13 @@ public class SpingBootSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-		.antMatchers("/usuario/*","/index/*", "/OlvidoContrasena/*", "/css/*","/js/*","/assets/*", "/img/*","/bootstrap5-website-master/*","/bootstrap-4.6.0-dist/*","/bootstrap-5.0.2-dist/*")
+		.antMatchers("/usuario/*","/index/*", "/OlvidoContrasena/*", "/css/*","/js/*","/assets/*", "/img/*","/bootstrap5-website-master/*","/bootstrap-4.6.0-dist/*","/bootstrap-5.0.2-dist/*", "Reports/*")
 	    .permitAll()
 	    .antMatchers("/dashboard-Admin","/Seguimiento/Administrador/**","/Inventarios/Administrador/**","/Asistencia/Administrador/**").hasRole("ADMINISTRADOR")
 		.antMatchers("/dashboard-Empleado","/Seguimiento/Empleado/**","/Inventarios/Empleado/*","/Asistencia/Empleado/**").hasRole("EMPLEADO")
 		.antMatchers("/dashboard-Jefe","/Seguimiento/JefeArea/**","/Asistencia/JefeArea/**").hasRole("JEFE DE AREA")
 		.antMatchers("/dashboard-Agente","/Asistencia/Agentes/**").hasRole("AGENTE TECNICO")
+		
 		.and().formLogin().loginPage("/login")
 		.permitAll().defaultSuccessUrl("/acceder");
 	}
